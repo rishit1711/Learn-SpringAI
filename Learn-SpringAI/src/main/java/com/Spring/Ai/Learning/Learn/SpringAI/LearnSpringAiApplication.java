e1eea0bed37da3d1da0a7e181e8e0d055d0840d5
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-24 22:48:14 +0530

    Initial commit

---
+++ Learn-SpringAI/src/main/java/com/Spring/Ai/Learning/Learn/SpringAI/LearnSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.Ai.Learning.Learn.SpringAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringAiApplication.class, args);
+	}
+
+}
---
+++ Learn-SpringAI/src/test/java/com/Spring/Ai/Learning/Learn/SpringAI/LearnSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.Ai.Learning.Learn.SpringAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
