404b34c59ea4a2711125c2150edb4a939638a51d
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-25 21:49:58 +0530

    Setup Config

---
+++ Learn-SpringAI/src/main/java/com/Spring/Ai/Learning/Learn/SpringAI/config/AiConfig.java
@@ -0,0 +1,16 @@
+package com.Spring.Ai.Learning.Learn.SpringAI.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder){
+        return builder.build();
+    }
+
+
+}
