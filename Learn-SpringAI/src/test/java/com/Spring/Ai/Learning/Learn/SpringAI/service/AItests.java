101815912a7f09af91e9a31c1373cc7257de4c09
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-26 23:34:17 +0530

    Set Up Gemini Api

--- Learn-SpringAI/src/test/java/com/Spring/Ai/Learning/Learn/SpringAI/service/AItests.java
+++ Learn-SpringAI/src/test/java/com/Spring/Ai/Learning/Learn/SpringAI/service/AItests.java
@@ -37,7 +37,7 @@ public class AItests {
 
     @Test
     public void askLLM(){
-        var res = aiService.askAI("Is java Future Dead");
+        var res = aiService.askAI("what is my name");
         System.out.println(res);
     }
 
