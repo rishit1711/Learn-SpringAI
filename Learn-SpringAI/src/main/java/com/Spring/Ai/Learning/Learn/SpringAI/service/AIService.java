cbc40e813c5a197f0c896aaaaa19be1a3024cdb8
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-29 16:20:32 +0530

    RAG Implementaion

--- Learn-SpringAI/src/main/java/com/Spring/Ai/Learning/Learn/SpringAI/service/AIService.java
+++ Learn-SpringAI/src/main/java/com/Spring/Ai/Learning/Learn/SpringAI/service/AIService.java
@@ -2,23 +2,18 @@ package com.Spring.Ai.Learning.Learn.SpringAI.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.embedding.Embedding;
 import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 import org.springframework.ai.document.Document;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AIService {
     private final ChatClient chatClient;
-
     private final VectorStore vectorStore;
     private final EmbeddingModel embeddingModel;
     public void ingest(){
@@ -56,51 +51,7 @@ public class AIService {
                 )
         );
     }
-    public String askAI(String question) {
 
-        String template = """
-        You are a helpful AI assistant.
-
-        Use the retrieved context to answer the user's question.
-
-        Rules:
-        - Prefer information from the provided context.
-        - Never invent facts.
-        - If the answer is not present, say you don't have enough information.
-        - Be clear and concise.
-        - Preserve technical terminology.
-
-        Context:
-        {context}
-        """;
-
-        List<Document> documents = vectorStore.similaritySearch(
-                SearchRequest.builder()
-                        .query(question)
-                        .topK(2)
-                        .build()
-        );
-
-        if (documents.isEmpty()) {
-            return "I don't have enough information to answer that question.";
-        }
-
-        String context = documents.stream()
-                .map(Document::getText)
-                .collect(Collectors.joining("\n\n"));
-
-        PromptTemplate promptTemplate = new PromptTemplate(template);
-
-        String systemPrompt = promptTemplate.render(
-                Map.of("context", context)
-        );
-
-        return chatClient.prompt()
-                .system(systemPrompt)
-                .user(question)
-                .call()
-                .content();
-    }
     public List<Document> similaritySearch(String text){
         return  vectorStore.similaritySearch(text);
     }
