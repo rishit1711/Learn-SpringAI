6bb828fdd2cc29c873a8b891fe71414ec1166ed3
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-30 10:05:39 +0530

    Learning Tool Calling

---
+++ Learn-SpringAI/src/main/java/com/Spring/Ai/Learning/Learn/SpringAI/tool/TravelTool.java
@@ -0,0 +1,22 @@
+package com.Spring.Ai.Learning.Learn.SpringAI.tool;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TravelTool {
+    @Tool(description = "Get Weather Information about the City")
+    public String getWeather(@ToolParam(description = "City name for which to get Information",required = true) String city){
+        switch (city){
+            case "Delhi":
+                return "24 Degree Celcius(Sunny)";
+            case "London":
+                return "18 Degree Celcius(Windy)";
+            case "Mumbai":
+                return "21 Degree Celcius(Humid)";
+            default:return "Could not Process";
+        }
+
+    }
+}
