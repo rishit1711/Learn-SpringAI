677ddcb61ea5df3ff4be2cb2d4438409374398af
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-30 10:35:09 +0530

    Learning Tool Calling

--- Learn-SpringAI/src/main/java/com/Spring/Ai/Learning/Learn/SpringAI/tool/TravelTool.java
+++ Learn-SpringAI/src/main/java/com/Spring/Ai/Learning/Learn/SpringAI/tool/TravelTool.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TravelTool {
+    // creating a tool and spring ai will add this tool into tool registry
     @Tool(description = "Get Weather Information about the City")
     public String getWeather(@ToolParam(description = "City name for which to get Information",required = true) String city){
         switch (city){
